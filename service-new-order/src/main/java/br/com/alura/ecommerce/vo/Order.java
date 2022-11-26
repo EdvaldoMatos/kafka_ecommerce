80c922f5cd855d1b7cdf388332bb50b8ac718394
Edvaldo S de Matos <edvaldo.santiago@gmail.com>
2022-11-26 18:53:20 -0300

    Finalizando o primeiro modulo kafka

---
+++ service-new-order/src/main/java/br/com/alura/ecommerce/vo/Order.java
@@ -0,0 +1,17 @@
+package br.com.alura.ecommerce.vo;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Order {
+
+    private String userId;
+    private String orderId;
+    private BigDecimal value;
+}
